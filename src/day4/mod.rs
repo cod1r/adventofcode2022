de2434659a033ebeb4fa30426f614b1b97c118b5
cod1r <jasonho353@gmail.com>
2022-12-05 11:11:26 -0600

    day5 done

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,27 +1,29 @@
 pub fn day4() {
     let bytes = include_bytes!("input.txt");
-    let answers = bytes[0..bytes.len() - 1].split(|b| *b == b'\n').fold((0, 0), |acc, elem| {
-        let mut parts = elem
-            .split(|byte| *byte == b',' || *byte == b'-')
-            .map(|ee| {
-                ee.iter()
-                    .fold(String::new(), |acc, e| acc + &(*e as char).to_string())
-            })
-            .map(|s| s.parse::<u8>().expect("a number"));
-        let one = parts.next().expect("some parts are not correct");
-        let two = parts.next().expect("some parts are not correct");
-        let three = parts.next().expect("some parts are not correct");
-        let four = parts.next().expect("some parts are not correct");
-        let mut first_part = acc.0;
-        let mut second_part = acc.1;
-        if one >= three && one <= four || three >= one && three <= two {
-            second_part += 1;
-        }
-        if (one >= three && two <= four) || (three >= one && four <= two) {
-            first_part += 1;
-        }
-        (first_part, second_part)
-    });
+    let answers = bytes[0..bytes.len() - 1]
+        .split(|b| *b == b'\n')
+        .fold((0, 0), |acc, elem| {
+            let mut parts = elem
+                .split(|byte| *byte == b',' || *byte == b'-')
+                .map(|ee| {
+                    ee.iter()
+                        .fold(String::new(), |acc, e| acc + &(*e as char).to_string())
+                })
+                .map(|s| s.parse::<u8>().expect("a number"));
+            let one = parts.next().expect("some parts are not correct");
+            let two = parts.next().expect("some parts are not correct");
+            let three = parts.next().expect("some parts are not correct");
+            let four = parts.next().expect("some parts are not correct");
+            let mut first_part = acc.0;
+            let mut second_part = acc.1;
+            if one <= four && three <= two {
+                second_part += 1;
+            }
+            if (one >= three && two <= four) || (three >= one && four <= two) {
+                first_part += 1;
+            }
+            (first_part, second_part)
+        });
     println!("day4");
     println!("{}\n{}", answers.0, answers.1);
 }
---
+++ src/day5/mod.rs
@@ -0,0 +1,57 @@
+pub fn day5() {
+    let bytes = include_bytes!("input.txt");
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9];
+    let mut end: usize = 0;
+    let mut stack: usize = 0;
+    for idx in (0..bytes.len()).step_by(4) {
+        let one = bytes[idx];
+        let two = bytes[idx + 1];
+        if two == '1' as u8 {
+            end = idx;
+            while bytes[end] != b'\n' {
+                end += 1;
+            }
+            end += 2;
+            break;
+        }
+        let three = bytes[idx + 2];
+        if one == '[' as u8 && two >= 'A' as u8 && two <= 'Z' as u8 && three == ']' as u8 {
+            stacks[stack].push(two as char);
+        }
+        stack = (stack + 1) % 9;
+    }
+    for s in &mut stacks {
+        s.reverse();
+    }
+    let mut stacks2 = Vec::new();
+    for s in &stacks {
+        stacks2.push(s.clone());
+    }
+    let string_lossy = String::from_utf8_lossy(&bytes[end..]);
+    assert!(string_lossy.as_bytes()[0] != b'\n');
+    let string = string_lossy.trim().split("\n");
+    for part in string {
+        let mut parts = part.split(" ");
+        let one = parts.nth(1).unwrap().parse::<usize>().unwrap();
+        let two = parts.nth(1).unwrap().parse::<usize>().unwrap();
+        let three = parts.nth(1).unwrap().parse::<usize>().unwrap();
+        for _ in 0..one {
+            let t = stacks[two - 1].pop().unwrap();
+            stacks[three - 1].push(t);
+        }
+        for count in 0..one {
+            let t = stacks2[two - 1][stacks2[two - 1].len() - (one - count)];
+            stacks2[three - 1].push(t);
+        }
+        for _ in 0..one {
+            stacks2[two - 1].pop();
+        }
+    }
+    for s in stacks {
+        print!("{}", s.last().unwrap());
+    }
+    println!();
+    for s in stacks2 {
+        print!("{}", s.last().unwrap());
+    }
+}
